8df04ba3bf425585c3b72c98d6adb93348a3a0ec
Markos Markides <136795075+MarkosMarkides@users.noreply.github.com>
2025-01-31 21:20:55 +0200

    Update gym_reservation_bot.py

--- gym_reservation_bot.py
+++ gym_reservation_bot.py
@@ -50,7 +50,7 @@ try:
         'Upgrade-Insecure-Requests': '1',
         'Origin': 'https://applications2.ucy.ac.cy',
         'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundaryTB670PeCHazqAW42',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.6613.120 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/53.36 (KHTML, like Gecko) Chrome/128.0.6613.120 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
         'Sec-Fetch-Site': 'same-origin',
         'Sec-Fetch-Mode': 'navigate',
